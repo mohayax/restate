58ffd8ff6e0cb104508394cbd0a4f6b1fd3cf55a
Muhammad Yakubu <mohayax02@gmail.com>
2025-02-17 20:36:39 +0100

    profile initial

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,16 +1,25 @@
-import { Text, View } from "react-native";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-rubik-extrabold my-10">Welcome to Restate!</Text>
-
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full"/>
+            <View className="flex flex-col items-start">
+              <Text className="text-xs text-black-100  font-rubik">Good Morning</Text>
+              <Text className="font-rubik-Medium text-base text-black-300">Muhammad</Text>
+            </View>
+          </View>
+          <View>
+            <Image source={icons.bell} className="size-6"/>
+        </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,11 +1,33 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import images from '@/constants/images'
+import icons from '@/constants/icons'
 
 const Profile = () => {
+
+  const handleLogout = async () => {}
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className='h-full bg-white'>
+      <ScrollView 
+      showsVerticalScrollIndicator={false}
+      contentContainerClassName='pb-32 px-7'
+      >
+        <View className='flex flex-row justify-between mt-5'>
+          <Text className='text-xl font-rubik-bold'>Profile</Text>
+          <Image source={icons.bell} className='size-5'/>
+        </View>
+        <View className='flex flex-row justify-center mt-5'>
+          <View className='flex flex-col items-center relative mt-5'>
+            <Image source={images.avatar} className='size-44 rounded-full'/>
+            <TouchableOpacity className='absolute bottom-11 right-2'>
+              <Image source={icons.edit} className='size-9 '/>
+            </TouchableOpacity>
+            <Text className="font-rubik-Medium text-base text-black-300">Muhammad</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
